5e27655988826696562b73b2655682a44aee5c53
macluiggy <ferrinluiggy@gmail.com>
2021-10-07 10:20:11 -0500

    updated

--- index.js
+++ index.js
@@ -2,15 +2,26 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.Pw, { useNewUrlParser: true }, { useUnifiedTopology: true });
 
 app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(__dirname + '/public'))
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+let responseObject = {}
+app.post('/api/users', (req, res) => {
+  let { username } = req.body;
+  responseObject['username'] = username;
 
-
+  res.json(responseObject)
+})
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
