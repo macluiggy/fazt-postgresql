74c0a71391944204b5245224053e8db58ae26547
macluiggy <ferrinluiggy@gmail.com>
2021-11-25 16:46:49 -0500

    adding create function callback

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,19 +1,23 @@
 const express = require("express");
+//model
+const User = require("../models/user.model")
 
 const router = express.Router();
-
-router.route("/api/users").post((req, res) => {
-  // //console.log(req.body)
-  let { username } = req.body;
-  let newUser = new User({ username: username });
-  newUser.save((error, savedUser) => {
-    if (!error) {
-      let responseObject = {};
-      responseObject["username"] = savedUser.username;
-      responseObject["_id"] = savedUser.id;
-      res.json(responseObject);
-    }
-  });
-});
+//controllers
+const { create } = require("../controllers/user.controller");
+// router.route("/api/users").post((req, res) => {
+//   // //console.log(req.body)
+//   let { username } = req.body;
+//   let newUser = new User({ username: username });
+//   newUser.save((error, savedUser) => {
+//     if (!error) {
+//       let responseObject = {};
+//       responseObject["username"] = savedUser.username;
+//       responseObject["_id"] = savedUser.id;
+//       return res.status(200).json(responseObject);
+//     }
+//   });
+// });
+router.route("/api/users").post(create);
 
 module.exports = router
