b41ef97bfb4da3140eed66cf4baa042e4ca6a010
macluiggy <ferrinluiggy@gmail.com>
2021-11-25 19:01:37 -0500

    fixed: bug when username is not provided

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -3,6 +3,10 @@ var { User } = require("../models/user.model");
 const create = async (req, res) => {
   // //console.log(req.body)
   let { body } = req;
+  if (!body.username) {
+    console.log(body);
+    return res.status(400).json({ message: "Username must be provided"})
+  }
   let newUser = new User(body);
   try {
     await newUser.save((error, savedUser) => {
