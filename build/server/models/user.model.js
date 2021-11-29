1f8ddef8cf280db4cfb174c94ac53e55f4957dfa
macluiggy <ferrinluiggy@gmail.com>
2021-11-29 14:42:51 -0500

    jdjd

--- build/server/models/user.model.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-var mongoose = require("mongoose");
-var exerciseSessionSchema = require("./session.model").exerciseSessionSchema;
-var userSchema = mongoose.Schema({
-    username: { type: String, required: true },
-    log: [exerciseSessionSchema],
-});
-var User = mongoose.model("User", userSchema);
-module.exports = { User: User };
